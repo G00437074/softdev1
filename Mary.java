2a8b46ce1a38847306d02a4121c6d48e3923513b
G00437074 <145337473+G00437074@users.noreply.github.com>
2023-10-03 09:52:41 +0100

    Add files via upload

---
+++ Mary.java
@@ -0,0 +1,15 @@
+public class Mary{
+        public static void main(String[]  args) {
+            String name = "Mary";
+            int age = 20;
+            String hobby = "running";
+
+            System.out.println(name + " is " + age + " years old. Her favourite hobby is " + hobby);
+
+            age = 21;
+
+            System.out.println("Forgot, it was Marys birthday yesterday, so updating her details as follows:");
+            System.out.println(name + " is " + age + " years old. Her favourite hobby is " + hobby);
+            
+        }
+}
