a6c6947a512a6da2d53f30eaeaead93e903b2cfd
G00437074 <145337473+G00437074@users.noreply.github.com>
2023-10-03 09:08:49 +0100

    Add files via upload

---
+++ PetrolReceipt.java
@@ -0,0 +1,16 @@
+public class PetrolReceipt{
+        public static void main(String[] args) {
+            System.out.println("+------------------------+");
+            System.out.println("|                        |");
+            System.out.println("|     Local Garage       |");
+            System.out.println("|  12-OCT-2021  09:10pm  |");
+            System.out.println("|                        |");
+            System.out.println("|  Litres:     44.00     |");
+            System.out.println("|  Price/Litre: 1.15     |");
+            System.out.println("|                        |");
+            System.out.println("|  Fuel Total: 50.60     |");
+            System.out.println("|                        |");
+            System.out.println("+------------------------+");
+    
+        }
+}
---
+++ PrimTypes.java
@@ -0,0 +1,28 @@
+public class PrimTypes {
+        public static void main(String[] args){
+            boolean tf = true;
+            System.out.println("Boolean: " + tf);
+
+            char c = 'x';
+            System.out.println("char: " + c);
+
+            byte b = 100;
+            System.out.println("Byte: " + b);
+
+            short s = 32000;
+            System.out.println("Short: " + s);
+
+            int i = 2147483647;
+            System.out.println("Int: " + i);
+
+            long l = 2147483649l;
+            System.out.println("long: " + l);
+
+            float f = 32.37f;
+            System.out.println("float: " + f);
+
+            double d = 65.789;
+            System.out.println("Double: " + d);
+        
+        }
+}
