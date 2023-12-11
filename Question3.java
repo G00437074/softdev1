5890ba7152ab1b66a11d7dd67b25833d7f94dc79
G00437074 <145337473+G00437074@users.noreply.github.com>
2023-12-11 09:59:41 +0000

    Add files via upload

---
+++ Question3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Question3{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int choice;
+       
+        input.close();
+    }   
+}
