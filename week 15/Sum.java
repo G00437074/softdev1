faf6c4dfac0d370764664f942ec65d0b20c2c719
G00437074 <145337473+G00437074@users.noreply.github.com>
2024-02-09 11:20:37 +0000

    Add files via upload

---
+++ week 15/Sum.java
@@ -0,0 +1,28 @@
+public class Sum{
+    public static void main(String[] args){
+        int total = 0;
+        int even = 0;
+        int odd = 0;
+
+        int i = 1;
+        while(i <= 100){
+            total += i;
+            if(i%2 == 0){
+                even += i;
+            }else{
+                odd += i;
+            }
+            i++;
+        }
+        System.out.println("Sum of all numbers from 1 to 100: " + total);
+        System.out.println("Sum of all numbers from 1 to 100: " + even);
+        System.out.println("Sum of all numbers from 1 to 100: " + odd);
+        if((odd - even) >= 0){
+            System.out.println("Sum of all numbers from 1 to 100: " + (odd- even));
+        }else{
+            System.out.println("Sum of all numbers from 1 to 100: " + (even - odd));
+        }
+        
+
+    }
+}
