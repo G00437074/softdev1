4637c62c33253cc6b16d00548416d3b87c069ab1
G00437074 <145337473+G00437074@users.noreply.github.com>
2024-02-09 11:18:25 +0000

    Add files via upload

---
+++ week 15/TwentyNums.java
@@ -0,0 +1,14 @@
+public class TwentyNums{
+    public static void main(String[] args){
+        int i = 0;
+        while(i <= 20){
+            if(i < 20){
+                System.out.print( i + " + ");
+            }else{
+                System.out.print(i);
+            }
+            
+            i++;
+        }
+    }
+}
