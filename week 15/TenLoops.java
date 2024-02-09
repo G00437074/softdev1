53960aadb31beefa8d8c5751af3e317dd3419e4d
G00437074 <145337473+G00437074@users.noreply.github.com>
2024-02-09 10:47:23 +0000

    Add files via upload

---
+++ week 15/TenLoops.java
@@ -0,0 +1,9 @@
+public class TenLoops{
+    public static void main(String[] args){
+        int x = 1;
+        while(x <= 10){
+            System.out.println("See me ten times");
+            x++;
+        }
+    }
+}
