f63a00f199d3bc6a73f6351056ac6f1dd8b1296b
G00437074 <145337473+G00437074@users.noreply.github.com>
2024-02-21 14:28:19 +0000

    Add files via upload

---
+++ week 17/Small.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Small{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int number = 0;
+        int small = 101;
+
+        for(int i = 1; i <= 5; i++){
+            System.out.println("Enter number " + i + ": ");
+            number = input.nextInt();
+            if(number < 101){
+                if(number < small){
+                    small = number;
+                }
+
+            }else{
+                System.out.println("Number enter must be 100 or less. Enter another number");
+                i--;
+            }
+
+        }
+        System.out.println("Smallest number entered is: "+ small);
+    }
+}
