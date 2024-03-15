afdae220a6de9836a25fdd1e4a70439b28e886dc
Adam Devlin <145337473+G00437074@users.noreply.github.com>
2024-03-15 08:07:20 +0000

    Add files via upload

---
+++ week 20/ProgramSkeleton.java
@@ -0,0 +1,16 @@
+//import java.util.Scanner;
+public class Program{
+    public static void main(String[] args) {
+        //Scanner input = new Scanner(System.in);
+        
+        // list variables
+
+        // inputs required
+
+        // if / else, and/or loops
+
+        // outputs
+
+        // input.close();
+    }
+}
