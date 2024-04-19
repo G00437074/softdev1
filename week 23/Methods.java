fbc88f824b6c58333c85b87b7b817d15263f6bcd
Adam Devlin <145337473+G00437074@users.noreply.github.com>
2024-04-19 09:38:36 +0100

    Add files via upload

---
+++ week 23/Methods.java
@@ -0,0 +1,25 @@
+public class Methods{
+    public static void main(String[] args) {
+        Methods m = new Methods();
+        m.method1();
+        m.method2(100);
+        m.method3(10, 2.5);
+        System.out.println(m.method4());
+        System.out.println(m.method5(5));
+    }
+    public void method1(){
+        System.out.println("method1");
+    }
+    public void method2(int x){
+        System.out.println("method2: " + x);
+    }
+    public void method3(int x, double y){
+        System.out.println("method3: " + x + " " + y);
+    }
+    public String method4(){
+        return "Hello from method4";
+    }
+    public double method5(int x){
+        return x * 0.25;
+    }
+}
